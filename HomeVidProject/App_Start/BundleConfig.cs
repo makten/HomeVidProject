5984ffb1b86e8569ae4fc657a4efa8d917b239eb
makten <hafizmakten@gmail.com>
2017-06-08 21:34:15 +0200

    initial commit

--- HomeVidProject/App_Start/BundleConfig.cs
+++ HomeVidProject/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace HomeVidProject
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-solar.css",
                       "~/Content/site.css"));
         }
     }
