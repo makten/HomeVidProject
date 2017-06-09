7cfee459d3b902dcb04f3b4e8139e7a84ce7df9e
makten <hafizmakten@gmail.com>
2017-06-10 00:01:49 +0200

    viewModel added

--- HomeVidProject/App_Start/RouteConfig.cs
+++ HomeVidProject/App_Start/RouteConfig.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Expression = Microsoft.Ajax.Utilities.Expression;
 
 namespace HomeVidProject
 {
@@ -13,8 +16,16 @@ namespace HomeVidProject
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapMvcAttributeRoutes();
-           
+            routes.MapMvcAttributeRoutes(); //Attributes routing --new
+
+            //routes.MapRoute(
+            //    name: "MoviesByReleaseDate",
+            //    url: "movies/released/{year}/{month}",
+            //    defaults: new { controller = "Movies", action = "ByReleaseDate" },
+            //    constraints: new { year = @"\d{4}", month = @"\d{2}"}
+            //    //constraints: new  { year = @"2015|2017" }
+            //    );
+
 
             routes.MapRoute(
                 name: "Default",
---
+++ HomeVidProject/Controllers/MoviesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HomeVidProject.Models;
+using HomeVidProject.ViewModels;
+
+namespace HomeVidProject.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies --ViewResult or ActionResult
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "The Book Of Eli!"};
+            var customers = new List<Customer>
+            {
+                new Customer() {Name = "John Doe" },
+                new Customer() { Name = "Kofi Kololo" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Id = " + id);
+
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content("Year = "+ year + " month =" + month);
+        }
+    }
+}
